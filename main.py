b357451debc7683c5d2f8cd5c9c0356bf6a5f7ba
Kyuwoong Lee <kwooler@gmail.com>
2022-10-10 18:46:14 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 words = input().split()
 keyword = input()
 
-for i in range(len(words)):
-	for j in range(len(keyword)):
-		if keyword[j] in words[i]:
-			print (words[i], end=' ')
-			break
-
-print ()
+# #####################
+# Make your coe
+# #####################
